e2ff8ea5f4dd041dd2d242d1edfef480e26751c2
Kolvinkst <einbenutzer1998@gmail.com>
2023-06-20 08:27:23 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DifferentDataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float myFloatValue = 10;
+            int myIntValue = (int) myFloatValue;
+            Console.WriteLine("myIntValue is " + myIntValue);
+            Console.ReadLine();
+        }
+    }
+}
